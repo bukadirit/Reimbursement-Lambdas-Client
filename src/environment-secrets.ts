294f58a8c4cdf48e619fc75f7a12b56fafdd0fae
Bukadiri Trawally <bukadiri.trawally@mail.citytech.cuny.edu>
2021-05-07 23:54:26 -0400

    Fixing env vars

---
+++ src/environment-secrets.ts
@@ -0,0 +1,7 @@
+export const Auth = {
+  mandatorySignIn: true,
+  region: 'us-east-2',
+  userPoolId: 'us-east-2_I5HK5eNN4',
+  userPoolWebClientId: '1cu1d9u7adlknf7eglerdaith0',
+  authenticationFlowType: 'USER_PASSWORD_AUTH',
+};
